c37c14cbd6af476a82021a35ecb4ffa5f25c98dc
danz4648 <danarputrapradana@yahoo.com>
2023-10-28 10:27:19 +0700

    initial commit

---
+++ src/test/java/com/danz/springnotes/SpringNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danz.springnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
